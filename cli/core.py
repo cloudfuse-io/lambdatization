d85ac6d155842dda0f17c0d52290c601787a9cd0
nbr <nanicpc@users.noreply.github.com>
2022-10-11 07:30:40 +0000

    raise for other client errors

--- cli/core.py
+++ cli/core.py
@@ -307,6 +307,8 @@ def dockerized(c, engine):
                 "LAMBDA_SECRET_ACCESS_KEY": creds.secret_key,
                 "LAMBDA_SESSION_TOKEN": creds.token,
             }
+        else:
+            raise Exit(message=error.response['Error']["Message"], code=1)
     compose = f"docker compose -f {TFDIR}/{engine}/build/docker-compose.yaml"
     c.run(f"{compose} down -v")
     c.run(f"{compose} build")
