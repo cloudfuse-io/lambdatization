e91327fd43aa2e4fb2988f24fd7d2d455d340754
nbr <nanicpc@users.noreply.github.com>
2022-10-17 16:26:51 +0000

    clean up lambda code

--- infra/ballista/build/lambda-handler.py
+++ infra/ballista/build/lambda-handler.py
@@ -1,13 +1,9 @@
 import os
 import subprocess
 import logging
-import base64
 import sys
-import io
-import json
 import shutil
 import time
-from threading import Thread
 
 
 logging.getLogger().setLevel(logging.DEBUG)
@@ -15,54 +11,6 @@ __stdout__ = sys.stdout
 
 IS_COLD_START = True
 
-class ReturningThread(Thread):
-    """A wrapper around the Thread class to actually return the threaded function
-    return value when calling join()"""
-
-    def __init__(self, target=None, args=()):
-        Thread.__init__(self, target=target, args=args)
-        self._return = None
-
-    def run(self):
-        if self._target is not None:
-            self._return = self._target(*self._args, **self._kwargs)
-
-    def join(self, *args):
-        Thread.join(self, *args)
-        return self._return
-
-
-class CommandException(Exception):
-    ...
-
-
-def hide_command_exception(func):
-    """Block printing on CommandException to avoid logging stderr twice"""
-
-    def func_wrapper(*args, **kwargs):
-        # reset stdout to its original value that we saved on init
-        sys.stdout = __stdout__
-        try:
-            return func(*args, **kwargs)
-        except CommandException as e:
-            # the error will be printed to a disposable buffer
-            sys.stdout = io.StringIO()
-            raise e
-
-    return func_wrapper
-
-
-def buff_and_print(stream, stream_name):
-    """Buffer and log every line of the given stream"""
-    buff = []
-    for l in iter(lambda: stream.readline(), b""):
-        line = l.decode("utf-8")
-        logging.info("%s: %s", stream_name, line.rstrip())
-        buff.append(line)
-    return "".join(buff)
-
-
-@hide_command_exception
 def handler(event, context):
     """An AWS Lambda handler that runs the provided command with bash and returns the standard output"""
     shutil.rmtree("/tmp", ignore_errors=True)
@@ -70,15 +18,11 @@ def handler(event, context):
     global IS_COLD_START
     is_cold_start = IS_COLD_START
     IS_COLD_START = False
-    # start scheduler and executor
+    # start scheduler
     process_s = subprocess.Popen(["/opt/ballista/ballista-scheduler"],
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # we need to spin up a thread to avoid deadlock when reading through output pipes
-    stderr_thread_s = ReturningThread(
-        target=buff_and_print, args=(process_s.stderr, "stderr")
-    )
-    stderr_thread_s.start()
-    logging.debug("stderr_thread_s.start()")
+    logging.debug("scheduler starts")
+    # wait till the scheduler is up and running
     return_code=1
     while return_code:
         res = subprocess.run(["nc", "-z", "localhost", "50050"], capture_output=True)
@@ -87,11 +31,7 @@ def handler(event, context):
         
     process_e = subprocess.Popen(["/opt/ballista/ballista-executor", "-c" ,"4"],
                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stderr_thread_e = ReturningThread(
-        target=buff_and_print, args=(process_e.stderr, "stderr")
-    )
-    stderr_thread_e.start()
-    logging.debug("stderr_thread_e.start()")
+    logging.debug("executor.starts")
     return_code=1
     while return_code:
         res = subprocess.run(["nc", "-z", "localhost", "50051"], capture_output=True)
