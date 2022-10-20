7665f43043306866f6129cc4b617fa54c44a353a
nbr <nanicpc@users.noreply.github.com>
2022-10-20 13:07:58 +0000

    style: black lint

--- cli/core.py
+++ cli/core.py
@@ -311,7 +311,12 @@ def dockerized(c, engine):
                 "LAMBDA_SESSION_TOKEN": creds.token,
             }
         else:
-            raise Exit(message=error.response.get("Error",{}).get("Message","Unidentified error getting AWS credentials"), code=1)
+            raise Exit(
+                message=error.response.get("Error", {}).get(
+                    "Message", "Unidentified error getting AWS credentials"
+                ),
+                code=1,
+            )
     compose = f"docker compose -f {TFDIR}/{engine}/build/docker-compose.yaml"
     c.run(f"{compose} down -v")
     c.run(f"{compose} build")
