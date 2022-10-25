7a1d6ebf045f1a3f5c9c047d0579d70c97ffcb83
nbr <nanicpc@users.noreply.github.com>
2022-10-25 08:07:06 +0000

    chore(cli): add description

--- cli/plugins/ballista.py
+++ cli/plugins/ballista.py
@@ -6,6 +6,7 @@ import core
 
 @task
 def lambda_example(c, path_to_folder="nyc-taxi/2019/01"):
+    """CREATE EXTERNAL TABLE and find out SUM(trip_distance) GROUP_BY payment_type"""
     ballista_cmd = f"""
 CREATE EXTERNAL TABLE trips STORED AS PARQUET
 LOCATION 's3://{core.bucket_name(c)}/{path_to_folder}/';
