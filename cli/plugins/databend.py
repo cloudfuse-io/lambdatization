5e8c2115e2e444821c4a1966e8d94a836706b957
Remi Dettai <rdettai@gmail.com>
2022-10-26 14:05:46 +0000

    fix(monitoring): remove IF NOT EXIST in databend

--- cli/plugins/databend.py
+++ cli/plugins/databend.py
@@ -9,7 +9,7 @@ def lambda_example(c, json_output=False, month="01"):
     """SUM(trip_distance) GROUP_BY payment_type"""
     # NOTE: __RUNTIME_PROVIDED__ is interpolated by the handler with actual credentials
     sql = f"""
-CREATE TRANSIENT TABLE IF NOT EXISTS taxi2019{month}
+CREATE TRANSIENT TABLE taxi2019{month}
 (
     payment_type VARCHAR,
     trip_distance FLOAT
