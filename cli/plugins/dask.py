6323e6f0a6bf7ef7a0d426a32a7ce2f76f92b34c
Remi Dettai <rdettai@gmail.com>
2022-10-26 14:07:01 +0000

    feat(dask): works locally but not on lambda

---
+++ cli/plugins/dask.py
@@ -0,0 +1,22 @@
+"""Dask on AWS Lambda"""
+
+from invoke import task
+import core
+
+
+@task(autoprint=True)
+def lambda_example(c, json_output=False, month="01"):
+    """SUM(trip_distance) GROUP_BY payment_type"""
+    sql = f"""
+CREATE TABLE nyctaxi WITH (
+    location = "s3://{core.bucket_name(c)}/nyc-taxi/2019/{month}/*",
+    format = "parquet"
+);
+
+SELECT payment_type, SUM(trip_distance) 
+FROM nyctaxi
+GROUP BY payment_type
+"""
+    if not json_output:
+        print(sql)
+    return core.run_lambda(c, "dask", sql, json_output=json_output)
---
+++ infra/runtime/dask/build/lambda-handler.py
@@ -0,0 +1,91 @@
+import os
+import logging
+import base64
+import sys
+import time
+import re
+import hashlib
+import dask.dataframe as dd
+from dask_sql import Context
+from distributed import LocalCluster
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+IS_COLD_START = True
+CLUSTER = None
+CONTEXT = None
+
+
+def init():
+    global CLUSTER, CONTEXT
+    CLUSTER = LocalCluster(n_workers=1, processes=True)
+    CONTEXT = Context()
+
+
+def query(sql):
+    """Splits the sql statements and return the result of the last one"""
+    resp = "Empty response"
+    for sql in sql.split(";"):
+        stripped_sql = sql.strip()
+        if stripped_sql != "":
+            plan = CONTEXT.sql(sql.strip())
+            # CREATE TABLE statements return None as plan
+            if plan is not None:
+                resp = str(CONTEXT.sql(sql.strip()).compute())
+    return resp
+
+
+def handler(event, context):
+    """An AWS Lambda handler that runs the provided command with bash and returns the standard output"""
+    global IS_COLD_START
+
+    is_cold_start = IS_COLD_START
+    IS_COLD_START = False
+
+    start = time.time()
+
+    if is_cold_start:
+        init()
+    init_duration = time.time() - start
+
+    # input parameters
+    logging.debug("event: %s", event)
+    src_command = base64.b64decode(event["cmd"]).decode("utf-8")
+    logging.info("command: %s", src_command)
+    if "env" in event:
+        logging.info("env: %s", event["env"])
+        for (k, v) in event["env"].items():
+            os.environ[k] = v
+
+    resp = query(src_command)
+
+    result = {
+        "stdout": resp,
+        "stderr": "",
+        "env": event.get("env", {}),
+        "context": {
+            "cold_start": is_cold_start,
+            "handler_duration_sec": time.time() - start,
+            "init_duration_sec": init_duration,
+        },
+    }
+    return result
+
+
+if __name__ == "__main__":
+    query_str = f"""
+CREATE TABLE nyctaxi WITH (
+    location = "s3://{os.getenv("DATA_BUCKET_NAME")}/nyc-taxi/2019/01/*",
+    format = "parquet"
+);
+
+SELECT payment_type, SUM(trip_distance) 
+FROM nyctaxi
+GROUP BY payment_type
+"""
+    res = handler(
+        {"cmd": base64.b64encode(query_str.encode("utf-8"))},
+        {},
+    )
+    print(res)
