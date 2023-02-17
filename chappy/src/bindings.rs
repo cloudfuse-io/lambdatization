f4741be64d5a4026eaae6414da8c8bdedf8c26f3
Remi Dettai <rdettai@gmail.com>
2023-02-17 11:09:26 +0000

    feat: add debug target of bind and connect

--- chappy/src/bindings.rs
+++ chappy/src/bindings.rs
@@ -16,6 +16,15 @@ fn init_logger() {
         .ok();
 }
 
+unsafe fn debug_parse(addr: *const sockaddr, len: socklen_t) -> String {
+    let addr_stor = nix::sys::socket::SockaddrStorage::from_raw(addr, Some(len)).unwrap();
+    if let Some(addr) = addr_stor.as_sockaddr_in() {
+        return format!("{}:{}", Ipv4Addr::from(addr.ip()), addr.port());
+    } else {
+        String::from("not-ipv4")
+    }
+}
+
 lazy_static! {
     static ref LIBC_LOADED: libloading::Library =
         unsafe { libloading::Library::new("/lib/x86_64-linux-gnu/libc.so.6").unwrap() };
@@ -33,7 +42,7 @@ pub unsafe extern "C" fn connect(sockfd: c_int, addr: *const sockaddr, len: sock
         Some(addr_in) => {
             let new_addr = request_punch(sockfd, addr_in);
             debug!(
-                "Calling libc.connect({}, {}, {}) instead of ({}, {}, {})",
+                "Calling libc.connect({}, {}:{}) instead of ({}, {}:{})",
                 sockfd,
                 Ipv4Addr::from(new_addr.ip()),
                 new_addr.port(),
@@ -49,7 +58,14 @@ pub unsafe extern "C" fn connect(sockfd: c_int, addr: *const sockaddr, len: sock
             }
             code
         }
-        None => libc_connect(sockfd, addr, len),
+        None => {
+            debug!(
+                "Calling libc.connect({}, {})",
+                sockfd,
+                debug_parse(addr, len)
+            );
+            libc_connect(sockfd, addr, len)
+        }
     }
 }
 
@@ -66,7 +82,7 @@ pub unsafe extern "C" fn bind(sockfd: c_int, addr: *const sockaddr, len: socklen
         Some(addr_in) => {
             let new_addr = register(sockfd, addr_in);
             debug!(
-                "Calling libc.bind({}, {}, {}) instead of ({}, {}, {})",
+                "Calling libc.bind({}, {}:{}) instead of ({}, {}:{})",
                 sockfd,
                 Ipv4Addr::from(new_addr.ip()),
                 new_addr.port(),
@@ -80,37 +96,36 @@ pub unsafe extern "C" fn bind(sockfd: c_int, addr: *const sockaddr, len: socklen
             }
             code
         }
-        None => libc_bind(sockfd, addr, len),
+        None => {
+            debug!("Calling libc.bind({}, {})", sockfd, debug_parse(addr, len));
+            libc_bind(sockfd, addr, len)
+        }
     }
 }
 
 /////// Interception of accept() for debugging purposes ///////
 
-type AcceptSymbol<'a> =
-    libloading::Symbol<'a, unsafe extern "C" fn(c_int, *const sockaddr, socklen_t) -> c_int>;
+// #[no_mangle]
+// pub unsafe extern "C" fn accept(sockfd: c_int, addr: *mut sockaddr, len: *mut socklen_t) -> c_int {
+//     accept4(sockfd, addr, len, 0)
+// }
 
-#[no_mangle]
-pub unsafe extern "C" fn accept(sockfd: c_int, addr: *const sockaddr, len: socklen_t) -> c_int {
-    init_logger();
-    debug!("Entering interception accept({})", sockfd);
-    let libc_accept: AcceptSymbol = LIBC_LOADED.get(b"accept").unwrap();
-    libc_accept(sockfd, addr, len)
-}
+// type Accept4Symbol<'a> = libloading::Symbol<
+//     'a,
+//     unsafe extern "C" fn(c_int, *mut sockaddr, *mut socklen_t, c_int) -> c_int,
+// >;
 
-type Accept4Symbol<'a> =
-    libloading::Symbol<'a, unsafe extern "C" fn(c_int, *const sockaddr, socklen_t, c_int) -> c_int>;
-
-#[no_mangle]
-pub unsafe extern "C" fn accept4(
-    sockfd: c_int,
-    addr: *const sockaddr,
-    len: socklen_t,
-    flags: c_int,
-) -> c_int {
-    init_logger();
-    debug!("Entering interception accept4({})", sockfd);
-    let libc_accept4: Accept4Symbol = LIBC_LOADED.get(b"accept4").unwrap();
-    let accept_res = libc_accept4(sockfd, addr, len, flags);
-    debug!("accept4({})->{}", sockfd, accept_res);
-    accept_res
-}
+// #[no_mangle]
+// pub unsafe extern "C" fn accept4(
+//     sockfd: c_int,
+//     addr: *mut sockaddr,
+//     len: *mut socklen_t,
+//     flags: c_int,
+// ) -> c_int {
+//     init_logger();
+//     debug!("Entering interception accept4({})", sockfd);
+//     let libc_accept4: Accept4Symbol = LIBC_LOADED.get(b"accept4").unwrap();
+//     let accept_res = libc_accept4(sockfd, addr, len, flags);
+//     debug!("accept4({})->{}", sockfd, accept_res);
+//     accept_res
+// }
