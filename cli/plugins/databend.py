34c6ccecae5c6d7e11749f01a38e6a79b6c73702
Remi Dettai <rdettai@gmail.com>
2023-04-27 15:30:31 +0200

    Upgrade Databend version (#93)

--- cli/plugins/databend.py
+++ cli/plugins/databend.py
@@ -1,6 +1,7 @@
 """Databend on AWS Lambda"""
 
 import core
+from common import AWS_REGION
 from invoke import task
 
 
@@ -9,20 +10,16 @@ def lambda_example(c, json_output=False, month="01"):
     """SUM(trip_distance) GROUP_BY payment_type"""
     # NOTE: __RUNTIME_PROVIDED__ is interpolated by the handler with actual credentials
     sql = f"""
-CREATE TRANSIENT TABLE taxi2019{month}
-(
-    payment_type VARCHAR,
-    trip_distance FLOAT
-);
-
-COPY INTO taxi2019{month}
-  FROM 's3://{core.bucket_name(c)}/nyc-taxi/2019/{month}/'
-  credentials=(__RUNTIME_PROVIDED__)
-  pattern ='.*[.]parquet'
-  file_format = (type = 'PARQUET');
+CREATE STAGE IF NOT EXISTS taxi2019{month}
+URL = 's3://{core.bucket_name(c)}/nyc-taxi/2019/{month}/'
+CONNECTION = (
+__RUNTIME_PROVIDED__
+REGION = '{AWS_REGION()}'
+)
+FILE_FORMAT = (type = 'PARQUET');
 
 SELECT payment_type, SUM(trip_distance) 
-FROM taxi2019{month}
+FROM @taxi2019{month}
 GROUP BY payment_type;
 """
     if not json_output:
