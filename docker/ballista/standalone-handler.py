ed2e8bae7c134d9b47d0b952287c61605d3aeaa2
Remi Dettai <rdettai@gmail.com>
2023-06-02 12:27:58 +0200

    fix: ballista timing out because of debug logging (#100)

--- docker/ballista/standalone-handler.py
+++ docker/ballista/standalone-handler.py
@@ -3,17 +3,20 @@ import logging
 import os
 import socket
 import subprocess
-import sys
 import time
+import traceback
 from contextlib import closing
-from typing import List
+from dataclasses import dataclass
+from typing import Any
 
 logging.getLogger().setLevel(logging.INFO)
 
 IS_COLD_START = True
 
 
-def wait_for_socket(process_name: str, port: int):
+def wait_for_socket(process_name: str, port: int) -> bool:
+    """Return false as soon as a TCP server is reachable on the provided port.
+    Return true if it isn't reachable within timeout"""
     c = 0
     start_time = time.time()
     while True:
@@ -26,21 +29,65 @@ def wait_for_socket(process_name: str, port: int):
                 logging.info(msg)
                 break
         if duration >= 5:
-            raise Exception(f"{process_name} timed out after {c} connection attempts")
+            logging.error(f"{process_name} timed out after {c} connection attempts")
+            return True
         time.sleep(0.02)
+    return False
 
 
-def start_server(name: str, cmd: List[str], port: int):
-    subprocess.Popen(
+@dataclass
+class ProcessResult:
+    process: subprocess.Popen[bytes]
+    start_timeout: bool
+
+
+def start_server(name: str, cmd: list[str], port: int) -> ProcessResult:
+    srv_proc = subprocess.Popen(
         cmd,
-        stderr=sys.stderr,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
         bufsize=0,
     )
     logging.info(f"{name} starting...")
-    wait_for_socket(name, port)
+    is_timeout = wait_for_socket(name, port)
+    return ProcessResult(srv_proc, is_timeout)
 
 
-def run_cli(sql: str) -> tuple[str, str]:
+def init_scheduler() -> ProcessResult:
+    cmd = [
+        "/opt/ballista/ballista-scheduler",
+        "--sled-dir",
+        "/tmp/scheduler/sled",
+        "--bind-host",
+        "127.0.0.1",
+        "--bind-port",
+        "50050",
+        "--log-level-setting",
+        "DEBUG",
+    ]
+    return start_server("scheduler", cmd, 50050)
+
+
+def init_executor(scheduler_ip: str) -> ProcessResult:
+    cmd = [
+        "/opt/ballista/ballista-executor",
+        "--bind-host",
+        "127.0.0.1",
+        "--bind-port",
+        "50051",
+        "--scheduler-host",
+        scheduler_ip,
+        "--scheduler-port",
+        "50050",
+        "--concurrent-tasks",
+        "1",
+        "--log-level-setting",
+        "DEBUG",
+    ]
+    return start_server("executor", cmd, 50051)
+
+
+def run_cli(sql: str, timeout: float) -> tuple[str, str]:
     logging.info("cli starts")
     with open("/tmp/sql_query.tmp", "w") as tmp_sql:
         tmp_sql.write(sql)
@@ -60,43 +107,76 @@ def run_cli(sql: str) -> tuple[str, str]:
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
-    stdout, stderr = process_cli.communicate(input=sql.encode())
+    try:
+        stdout, stderr = process_cli.communicate(input=sql.encode(), timeout=timeout)
+    except subprocess.TimeoutExpired as e:
+        stdout = e.stdout
+        stderr = e.stderr
+    assert stdout is not None, "not None if PIPE specified"
+    assert stderr is not None, "not None if PIPE specified"
     return stdout.decode(), stderr.decode()
 
 
-def init():
-    sched_cmd = [
-        "/opt/ballista/ballista-scheduler",
-        "--sled-dir",
-        "/tmp/scheduler/sled",
-    ]
-    start_server("scheduler", sched_cmd, 50050)
-    start_server("executor", ["/opt/ballista/ballista-executor"], 50051)
+def stop_server(proc: subprocess.Popen[bytes]) -> tuple[str, str]:
+    if proc.poll() is None:
+        proc.kill()
+    assert proc.stdout is not None
+    assert proc.stderr is not None
+    return (proc.stdout.read().decode(), proc.stderr.read().decode())
 
 
-def handler(event, context):
-    """AWS Lambda handler"""
+def handle_event(event) -> dict[str, Any]:
     start = time.time()
     global IS_COLD_START
     is_cold_start = IS_COLD_START
     IS_COLD_START = False
-    if is_cold_start:
-        init()
-    src_command = base64.b64decode(event["query"]).decode("utf-8")
+
+    # TODO this restarts the servers at each time, which degrades warm starts
+    sched_proc = init_scheduler()
+    exec_proc = init_executor("127.0.0.1")
+
     init_duration = time.time() - start
+    cli_timeout_sec = 60
 
-    resp, logs = run_cli(src_command)
+    result = {}
+    if sched_proc.start_timeout or exec_proc.start_timeout:
+        result["startup_timeout"] = "true"
+        sched_stdout, sched_stderr = stop_server(sched_proc.process)
+        exec_stdout, exec_stderr = stop_server(exec_proc.process)
+    else:
+        query_start = time.time()
+        src_command = base64.b64decode(event["query"]).decode("utf-8")
+        cli_stdout, cli_stderr = run_cli(src_command, cli_timeout_sec)
+        result["query_duration_sec"] = time.time() - query_start
+        sched_stdout, sched_stderr = stop_server(sched_proc.process)
+        result["cli_resp"] = cli_stdout
+        result["cli_logs"] = cli_stderr
+        result["parsed_queries"] = [src_command]
+        exec_stdout, exec_stderr = stop_server(exec_proc.process)
 
-    result = {
-        "resp": resp,
-        "logs": logs,
-        "parsed_queries": [src_command],
-        "context": {
-            "cold_start": is_cold_start,
-            "handler_duration_sec": time.time() - start,
-            "init_duration_sec": init_duration,
-        },
+    result["sched_stdout"] = sched_stdout
+    result["sched_stderr"] = sched_stderr
+    result["exec_stdout"] = exec_stdout
+    result["exec_stderr"] = exec_stderr
+
+    result["context"] = {
+        "cold_start": is_cold_start,
+        "handler_duration_sec": time.time() - start,
+        "init_duration_sec": init_duration,
     }
+    return result
+
+
+def handler(event, context):
+    """AWS Lambda handler
+
+    event:
+    - query: str (base64)
+    """
+    try:
+        result = handle_event(event)
+    except Exception:
+        result = {"exception": traceback.format_exc()}
     return result
 
 
